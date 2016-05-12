524c78aaaec8294e6cabd64fa56ff4c52dc6faf4
bananvyhe <loadonden@yahoo.com>
2016-05-12 11:25:40 +0600

    final

--- app.rb
+++ app.rb
@@ -21,6 +21,14 @@ configure do
 	created_date DATE, 
 	content TEXT
 	)'
+
+	@db.execute 'CREATE TABLE IF NOT EXISTS Comments 
+	(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	created_date DATE, 
+	content TEXT,
+	post_id integer
+	)'
 end
 
 get '/' do
@@ -49,7 +57,25 @@ end
 get '/details/:post_id' do
 	post_id = params[:post_id]
 	results = @db.execute 'select * from Posts where id = ?', [post_id]
-	 
 	@row = results[0]
+	@comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
 	erb :details
+end
+
+post '/details/:post_id' do
+	post_id = params[:post_id]
+	content = params[:content]
+	@db.execute 'insert into Comments 
+		(
+			content, 
+			created_date, 
+			post_id
+		) 
+			values 
+		(
+			?, 
+			datetime(),
+			?
+			)', [content, post_id]
+	redirect to ('/details/' + post_id)
 end
